ea8c871548d9fc76cb1bb2d743813c13d38b95f1
anton <anton.filatov48@gmail.com>
2022-03-23 06:31:19 +0600

    Описание класса о переводе средств

--- 4 задание/5/5/Program.cs
+++ 4 задание/5/5/Program.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 
 namespace _5
 {
+    abstract class Transfer
+    {
+        public abstract void trans();
+    }
+    class Currency_exchange : Transfer
+    {
+        public override void trans()
+        {
+            Console.WriteLine("Обмен волют");
+        }
+        public void printStatus()
+        {
+            Console.WriteLine("Выполнено");
+        }
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
